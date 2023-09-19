bc594f42eba0410a84d7da30190d66a0da4605ce
Sergio Peralta <seperalta88@gmail.com>
2023-09-19 15:11:37 -0300

    Set up greetings function and state

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreetings } from '../store/greetings/greetingsSlice';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greetings = useSelector((state) => state.greetings);
+  console.log(greetings);
+
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, [dispatch]);
+
+  const message = greetings && greetings.greetings.message;
+
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Greeting;
